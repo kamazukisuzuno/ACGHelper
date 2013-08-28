ecfffb402c67ec2c4a6d4edbfc342f4b279fac61
suzuno <kamazukisuzuno@outlook.com>
2013-08-28 15:47:43 +0800

    1

---
+++ ACGHelper/src/main/java/suzuno/acghelper/main.java
@@ -0,0 +1,46 @@
+package suzuno.acghelper;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+/**
+ * Created by suzuno on 13-8-28.
+ */
+public class main extends Activity {
+
+    @Override
+    protected void onCreate(Bundle onSavedInstance){
+        super.onCreate(onSavedInstance);
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+    }
+
+    @Override
+    public void onRestart(){
+        super.onRestart();
+    }
+
+    @Override
+    public void onNewIntent(Intent intent){
+        super.onNewIntent(intent);
+    }
+
+    @Override
+    public void onPause(){
+        super.onPause();
+    }
+
+    @Override
+    public void onStop(){
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+    }
+}
